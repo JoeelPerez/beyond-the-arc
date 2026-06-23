307ddbb75c4f5f76c996932f78174bef5688bf01
Joeel71 <elnanito2002@gmail.com>
2026-06-23 23:06:55 +0200

    TeamName filter add on NBAMatches

--- src/model/nba_matches.py
+++ src/model/nba_matches.py
@@ -1,4 +1,15 @@
+TEAM_NAME_COLUMN = "teamName"
+
+
 class NBAMatches:
 
     def __init__(self, nba_matches_dataset):
         self.__nba_matches_dataset = nba_matches_dataset
+
+    def get_matches(self):
+        return self.__nba_matches_dataset
+
+    def filter_team_name(self, team_name):
+        self.__nba_matches_dataset = self.__nba_matches_dataset[
+            self.__nba_matches_dataset[TEAM_NAME_COLUMN] == team_name]
+        return self
--- test/nba_matches_test.py
+++ test/nba_matches_test.py
@@ -1,16 +1,17 @@
 import unittest
 
-from src.model.nba_matches import NBAMatches
 from src.readers_utils.csv_file_reader import CSVFileReader
 from src.readers_utils.nba_matches_parser import NBAMatchesParser
 
 
-class NBAMatchesParserTest(unittest.TestCase):
+class NBAMatchesTest(unittest.TestCase):
     file_path = "data/TeamStatistics.csv"
+    example_team_name = "Cavaliers"
 
-    def test_nba_matches_parser(self):
+    def test_nba_matches_team_name_filter(self):
         nba_matches = NBAMatchesParser(self.file_path, CSVFileReader()).parse()
-        self.assertIsInstance(nba_matches, NBAMatches)
+        nba_matches_dataframe = nba_matches.filter_team_name(self.example_team_name).get_matches()
+        assert(nba_matches_dataframe["teamName"].values[0] == self.example_team_name)
 
 
 if __name__ == '__main__':
